559be3982be426001f2d016de1664c970679e616
Muthahir26 <165277675+Muthahir26@users.noreply.github.com>
2024-06-15 10:37:12 +0530

    firstpush

---
+++ simpleproject/src/test/java/pack2/OrgModuleTest.java
@@ -0,0 +1,14 @@
+package pack2;
+
+import org.testng.annotations.Test;
+
+public class OrgModuleTest {
+	@Test
+	public void createOrg() {
+		System.out.println("execute createOrg");
+	}
+    @Test
+	public void editeOrg() {
+		System.out.println("execute editeOrg");
+	}
+}
